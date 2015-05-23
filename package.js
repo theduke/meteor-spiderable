a7b355beb13bb2dee6051767841186a07e8787c2
theduke <chris@theduke.at>
2015-05-23 06:05:45 +0200

    Initial commit with spiderable package.

---
+++ package.js
@@ -0,0 +1,29 @@
+Package.describe({
+  name: "theduke:spiderable-docker",
+  summary: "Makes the application crawlable to web spiders",
+  version: "1.0.8",
+  git: "https://github.com/theduke/spiderable"
+});
+
+Package.onUse(function (api) {
+  api.use('webapp', 'server');
+  api.use(['ddp'], 'client');
+  api.use(['callback-hook'], 'client');
+  api.use(['templating'], 'client');
+  api.use(['underscore'], ['client', 'server']);
+
+  api.export('Spiderable');
+
+  api.addFiles('spiderable.html', 'client');
+  api.addFiles('spiderable.js', ['client', 'server']);
+  api.addFiles('spiderable_server.js', 'server');
+  api.addFiles('spiderable_client.js', 'client');
+
+  api.addFiles('phantom_script.js', 'server', { isAsset: true });
+});
+
+Package.onTest(function (api) {
+  api.use(['spiderable', 'tinytest']);
+  api.addFiles('spiderable_client_tests.js', 'client');
+  api.addFiles('spiderable_server_tests.js', 'server');
+});
